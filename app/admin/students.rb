badbf1851e7443bc700d3e435caae3590f0a483b
Venkatesh Joshi <venkateshrj12@gmail.com>
2024-01-16 09:44:01 +0530

    edit for for student model

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -2,4 +2,13 @@ ActiveAdmin.register Student do
   permit_params :name, :date_of_birth, :gender
 
   filter :name
+
+  form do |f|
+    f.inputs 'Student Details' do
+      f.input :name
+      f.input :date_of_birth, as: :date_time_picker
+      f.input :gender
+    end
+    f.actions
+  end
 end
