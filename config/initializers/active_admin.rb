7d668aad9083d23115e68925021fa18a9b138a85
Venkatesh Joshi <venkateshrj12@gmail.com>
2024-01-18 18:03:09 +0530

    device compatible app update

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,6 +5,9 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Dharmika Shibira"
+  meta_tags_options = { viewport: 'width=device-width, initial-scale=1' }
+  config.meta_tags = meta_tags_options
+  config.meta_tags_for_logged_out_pages = meta_tags_options
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
