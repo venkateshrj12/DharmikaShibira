c06dab9e417acbdb4b8bd6cbffad5ace1c301662
adarrsh <68156814+Fucloea@users.noreply.github.com>
2024-02-09 17:56:31 +0530

    add cancan auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    
+    def access_denied(exception)
+        redirect_to admin_admin_users_path, alert: exception.message
+      end
 end
---
+++ db/migrate/20240209070239_add_role_to_admin_user.rb
@@ -0,0 +1,5 @@
+class AddRoleToAdminUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :admin_users, :role, :string
+  end
+end
