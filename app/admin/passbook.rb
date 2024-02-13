4351411e5553195784fcbc0520326fc65b8e641b
adarrsh <68156814+Fucloea@users.noreply.github.com>
2024-02-13 13:19:36 +0530

    add cashbook html version with csv

--- app/admin/passbook.rb
+++ app/admin/passbook.rb
@@ -1,55 +1,46 @@
-# app/admin/passbook.rb
-ActiveAdmin.register_page "Cashbook" do
-  content title: "Cashbook" do
-    columns do
-      column do
-        panel "Transactions" do
-          transactions = (Receipt.where(mode_of_payment: "cash") + Receipt.where(mode_of_payment: "bank") + Payment.where(mode_of_payment: "cash") + Payment.where(mode_of_payment: "bank")).sort_by(&:created_at)
+ActiveAdmin.register_page 'Cashbook' do
+  content title: 'Cashbook' do
+    per_page = 20
+    page = params[:page].to_i || 1
+    start_index =  ((page - 1) * per_page) > 0 ? ((page - 1) * per_page) : 0
+    end_index = start_index + per_page - 1
+    @transactions = (Receipt.where(mode_of_payment: ["cash", "bank"]) + Payment.where(mode_of_payment: ["cash", "bank"])).sort_by(&:created_at)[start_index..end_index]
 
-          running_total_cash = 0
-          running_total_bank = 0
+    total_cash = Receipt.where(mode_of_payment: "cash").sum(:seva_amount) - Payment.where(mode_of_payment: "cash").sum(:amount)
+    total_bank = Receipt.where(mode_of_payment: "bank").sum(:seva_amount) - Payment.where(mode_of_payment: "bank").sum(:amount)
+    total_balance = total_cash + total_bank
 
-          table_for transactions do
-            column("Date") do |transaction|
-              if transaction.is_a?(Receipt)
-                link_to transaction.created_at.to_date, admin_receipt_path(transaction)
-              else
-                link_to transaction.created_at.to_date, admin_payment_path(transaction)
-              end
-            end
-
-            column("Type") { |transaction| transaction.class.name }
-
-            column("Cash") do |transaction|
-              amount = transaction.class.name == "Receipt" ? transaction.seva_amount : -transaction.amount
-              transaction.mode_of_payment == "cash" ? amount : 0
-            end
-
-            column("Bank") do |transaction|
-              amount = transaction.class.name == "Receipt" ? transaction.seva_amount : -transaction.amount
-              transaction.mode_of_payment == "bank" ? amount : 0
-            end
-
-            column("Total Cash") { |transaction| running_total_cash += transaction.mode_of_payment == "cash" ? (transaction.class.name == "Receipt" ? transaction.seva_amount : -transaction.amount) : 0 }
-            column("Total Bank") { |transaction| running_total_bank += transaction.mode_of_payment == "bank" ? (transaction.class.name == "Receipt" ? transaction.seva_amount : -transaction.amount) : 0 }
-
-          end
-        end
-      end
-    end
-
-    columns do
-      column do
-        panel "Total Balance" do
-          total_cash = Receipt.where(mode_of_payment: "cash").sum(:seva_amount) - Payment.where(mode_of_payment: "cash").sum(:amount)
-          total_bank = Receipt.where(mode_of_payment: "bank").sum(:seva_amount) - Payment.where(mode_of_payment: "bank").sum(:amount)
-          total_balance = total_cash + total_bank
-
-          div "Total Cash: #{total_cash}"
-          div "Total Bank: #{total_bank}"
-          div "Total Balance: #{total_balance}"
-        end
-      end
-    end
+    total_records = (Receipt.where(mode_of_payment: ["cash", "bank"]) + Payment.where(mode_of_payment: ["cash", "bank"])).size
+    @total_pages = (total_records.to_f / per_page).ceil
+    render partial: 'cashbook_page', locals: { transactions: @transactions, page: page, per_page: per_page, total_pages: @total_pages, cash_total: total_cash, bank_total: total_bank, final_total: total_balance }
   end
+
+  page_action :export_cashbook_file do
+    @transactions = (Receipt.where(mode_of_payment: ["cash", "bank"]) + Payment.where(mode_of_payment: ["cash", "bank"])).sort_by(&:created_at)
+
+    csv_data = CSV.generate(headers: true) do |csv|
+      csv << ['Sl.No', "Type", "Name", "Date", "Receipts - Cash", "Receipts - Bank", "Payments - Cash", "Payments - Bank", "Total Cash", "Total Bank", "Total"]
+      running_total_bank = 0
+      running_total_cash = 0
+      serial_counter = 0
+
+      @transactions.each do |transaction|
+        csv << [
+          serial_counter += 1,
+          transaction.is_a?(Receipt) ? "Receipt" : "Payment",
+          transaction.voucher.voucher_name,
+          transaction.created_at.to_date,
+          transaction.mode_of_payment == "cash" && transaction.is_a?(Receipt) ? transaction.seva_amount : "",
+          transaction.mode_of_payment == "bank" && transaction.is_a?(Receipt) ? transaction.seva_amount : "",
+          transaction.mode_of_payment == "cash" && transaction.is_a?(Payment) ? -transaction.amount : "",
+          transaction.mode_of_payment == "bank" && transaction.is_a?(Payment) ? -transaction.amount : "",
+          running_total_cash += transaction.mode_of_payment == "cash" ? (transaction.is_a?(Receipt) ? transaction.seva_amount : -transaction.amount) : 0,
+          running_total_bank += transaction.mode_of_payment == "bank" ? (transaction.is_a?(Receipt) ? transaction.seva_amount : -transaction.amount) : 0,
+          running_total_bank + running_total_cash
+        ]
+      end
+    end
+    send_data csv_data, filename: "cashbook_data.csv"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: redirect('/admin')
+
+  namespace :admin do
+    get 'cashbook/export_cashbook_file', to: 'cashbook#export_cashbook_file', as: :export_cashbook_file
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
