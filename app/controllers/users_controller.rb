496df2cbf888f760714d2a5fcb6271a6d31ff297
Venkatesh Joshi <venkateshrj12@gmail.com>
2024-03-02 17:45:15 +0530

    controller to create admin account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  def create
+    @user = AdminUser.new(user_params)
+    if @user.save
+      render json: { message: "User created successfully" }
+    else
+      render json: { error: "User not created" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'cashbook/export_cashbook_file', to: 'cashbook#export_cashbook_file', as: :export_cashbook_file
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
